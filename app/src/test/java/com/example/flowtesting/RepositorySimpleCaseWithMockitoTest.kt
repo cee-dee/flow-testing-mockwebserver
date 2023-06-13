7d800aa04031dd591b56130696133fb0328cf432
Björn Kopiske <b.kopiske@iplabs.de>
2023-06-13 18:15:40 +0200

    Add mockito based test

---
+++ app/src/test/java/com/example/flowtesting/RepositorySimpleCaseWithMockitoTest.kt
@@ -0,0 +1,68 @@
+package com.example.flowtesting
+
+import app.cash.turbine.test
+import com.example.flowtesting.utils.MainDispatcherRule
+import com.example.flowtesting.utils.createFailingStubBackedRepository
+import com.example.flowtesting.utils.createSucceedingStubBackedRepository
+import com.google.common.truth.Truth.assertThat
+import junitparams.JUnitParamsRunner
+import junitparams.Parameters
+import junitparams.naming.TestCaseName
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(JUnitParamsRunner::class)
+@OptIn(ExperimentalCoroutinesApi::class)
+@Suppress("UnusedPrivateMember", "BlockingMethodInNonBlockingContext")
+class RepositorySimpleCaseWithMockitoTest {
+
+    @get:Rule
+    val mainDispatcherRule = MainDispatcherRule()
+
+    @TestCaseName("[{index}] {method}")
+    @Parameters(method = "provideIndexes")
+    @Test
+    fun `not flaky -- succeeding simple case`(iteration: Int) {
+        runTest {
+
+            val sut = createSucceedingStubBackedRepository()
+
+            sut.select(selector)
+
+            sut.data.test {
+                val item = awaitItem()
+                assertThat(item)
+                    .isEqualTo(Result.success(Data(value = "value1")))
+            }
+        }
+    }
+
+    @TestCaseName("[{index}] {method}")
+    @Parameters(method = "provideIndexes")
+    @Test
+    fun `not flaky -- succeeding connect exception case`(iteration: Int) {
+        runTest {
+
+            val sut = createFailingStubBackedRepository()
+
+            sut.select(selector)
+
+            sut.data.test {
+                val item = awaitItem()
+                assertThat(item.isFailure).isTrue()
+                assertThat(item.exceptionOrNull()).isInstanceOf(RuntimeException::class.java)
+            }
+        }
+    }
+
+    fun provideIndexes(): Collection<Int> {
+        return (1..50).toList()
+    }
+
+    companion object {
+        private const val selector = "999999"
+    }
+}
--- app/src/test/java/com/example/flowtesting/utils/RepositoryCreationUtils.kt
+++ app/src/test/java/com/example/flowtesting/utils/RepositoryCreationUtils.kt
@@ -1,7 +1,13 @@
 package com.example.flowtesting.utils
 
+import com.example.flowtesting.Data
+import com.example.flowtesting.DataService
 import com.example.flowtesting.Repository
+import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import org.mockito.ArgumentMatchers.anyString
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.`when`
 
 fun createRepository(
     mockWebServerUrl: String,
@@ -9,6 +15,30 @@ fun createRepository(
 ): Repository {
     return Repository(
         dataService = createRetrofitClient(mockWebServerUrl, useInstantOkHttpDispatcher),
+        dispatcher = UnconfinedTestDispatcher()
+    )
+}
+
+fun createSucceedingStubBackedRepository(): Repository {
+    val dataService = mock(DataService::class.java)
+    runBlocking {
+        `when`(dataService.loadData(anyString())).thenReturn(
+            Data("value1")
+        )
+    }
+    return Repository(
+        dataService = dataService,
+        dispatcher = UnconfinedTestDispatcher()
+    )
+}
+
+fun createFailingStubBackedRepository(): Repository {
+    val dataService = mock(DataService::class.java)
+    runBlocking {
+        `when`(dataService.loadData(anyString())).thenThrow(RuntimeException::class.java)
+    }
+    return Repository(
+        dataService = dataService,
         dispatcher = UnconfinedTestDispatcher()
     )
 }
