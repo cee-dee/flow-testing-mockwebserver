244f0cb25f8204d93fc73ff45abf5688250616b8
Björn Kopiske <b.kopiske@iplabs.de>
2023-06-13 18:15:50 +0200

    Remove comment

--- app/src/test/java/com/example/flowtesting/RepositoryConnectionIssueTest.kt
+++ app/src/test/java/com/example/flowtesting/RepositoryConnectionIssueTest.kt
@@ -127,38 +127,6 @@ class RepositoryConnectionIssueTest {
                 verifyFailingDueToConnectionIssue(item)
             }
         }
-
-        /*
-        While the RepositorySimpleCaseTest#`not flaky -- succeeding simple case using instant OkHttpDispatcher`
-        test, where no exceptions on the Network layer a thrown, reliably succeed using the instant execution
-        OkHttpDispatcher, in the exception case, this still doesn't suffice.
-
-        With some nasty Thread.sleep(20) it seems to work "reliably on my machine". Still, that's
-        a crappy half-baked solution.
-
-        The reason for this could be related to some code I found in Retrofit2:
-
-            /**
-             * Force the calling coroutine to suspend before throwing [this].
-             *
-             * This is needed when a checked exception is synchronously caught in a [java.lang.reflect.Proxy]
-             * invocation to avoid being wrapped in [java.lang.reflect.UndeclaredThrowableException].
-             *
-             * The implementation is derived from:
-             * https://github.com/Kotlin/kotlinx.coroutines/pull/1667#issuecomment-556106349
-             */
-            internal suspend fun Exception.suspendAndThrow(): Nothing {
-              suspendCoroutineUninterceptedOrReturn<Nothing> { continuation ->
-                Dispatchers.Default.dispatch(continuation.context, Runnable {
-                  continuation.intercepted().resumeWithException(this@suspendAndThrow)
-                })
-                COROUTINE_SUSPENDED
-              }
-            }
-
-          It is called in HttpServiceMethod#adapt whenever an Exception occurs during a
-          network request.
-       */
     }
 
     @After
